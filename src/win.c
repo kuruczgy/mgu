062ddddb41dc4ead3378c3ea6c8f33e19a77ce58
György Kurucz <kuruczgyurci@hotmail.com>
2021-01-27 22:48:18 +0100

    Move input event codes to a separate header

--- src/win.c
+++ src/win.c
@@ -8,6 +8,7 @@
 #include <assert.h>
 #include <platform_utils/log.h>
 #include <time.h>
+#include <mgu/input-event-codes.h>
 #if defined(__EMSCRIPTEN__)
 #include <emscripten.h>
 #include <emscripten/html5.h>
@@ -20,50 +21,6 @@
 #include <poll.h>
 #endif
 
-#if defined(__EMSCRIPTEN__) || defined(__ANDROID__)
-// copied straight from linux/input-event-codes.h
-
-#define KEY_1			2
-#define KEY_2			3
-#define KEY_3			4
-#define KEY_4			5
-#define KEY_5			6
-#define KEY_6			7
-#define KEY_7			8
-#define KEY_8			9
-#define KEY_9			10
-#define KEY_0			11
-
-#define KEY_Q			16
-#define KEY_W			17
-#define KEY_E			18
-#define KEY_R			19
-#define KEY_T			20
-#define KEY_Y			21
-#define KEY_U			22
-#define KEY_I			23
-#define KEY_O			24
-#define KEY_P			25
-#define KEY_A			30
-#define KEY_S			31
-#define KEY_D			32
-#define KEY_F			33
-#define KEY_G			34
-#define KEY_H			35
-#define KEY_J			36
-#define KEY_K			37
-#define KEY_L			38
-#define KEY_Z			44
-#define KEY_X			45
-#define KEY_C			46
-#define KEY_V			47
-#define KEY_B			48
-#define KEY_N			49
-#define KEY_M			50
-
-#else
-#include <linux/input-event-codes.h>
-#endif
 
 #if defined(__EMSCRIPTEN__)
 EM_JS(double, mgu_win_internal_init_resize_listener, (), {
@@ -496,7 +453,6 @@ static int32_t engine_handle_input(struct android_app* app,
 			>> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
 		action = action & AMOTION_EVENT_ACTION_MASK;
 		size_t count = AMotionEvent_getPointerCount(event);
-		pu_log_info("input action: %d, count: %d, index: %d\n", action, count, index);
 		for (size_t i = 0; i < count; ++i) {
 			int32_t id = AMotionEvent_getPointerId(event, i);
 			float x = AMotionEvent_getX(event, i);
